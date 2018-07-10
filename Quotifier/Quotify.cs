726c3fb08036cb870fff5d80b5f808d3e1472dfb
Jared Szymurski <jszymurski@frontierssi.com>
2018-07-10 13:06:47 -0400

    Add project files.

---
+++ Quotifier/Quotify.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Text;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Quotifier
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class Quotify
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("5b630bcd-c209-4053-b9f6-a571f9104e39");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Quotify"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private Quotify(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            this.package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static Quotify Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package)
+        {
+            Instance = new Quotify(package);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            var dte = (DTE2)ServiceProvider.GetService(typeof(DTE));
+            var selectedText = ((TextSelection)dte.ActiveDocument.Selection).Text;
+
+            ((TextSelection)dte.ActiveDocument.Selection).Text = TextParse.Quotify(selectedText);
+        }
+    }
+}
---
+++ Quotifier/TextParse.cs
@@ -0,0 +1,59 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quotifier
+{
+    public class TextParse
+    {
+        public static string Commafy(string selectedText)
+        {
+            var returnString = selectedText;
+
+            if (selectedText != null && selectedText != String.Empty)
+            {
+                var commafied = new StringBuilder();
+                foreach (var item in selectedText.Replace("\r", "").Split('\n'))
+                {
+                    var updatedItem = item;
+                    if (item != string.Empty)
+                    {
+                        updatedItem = String.Format(@"{0}{1}", item, ",");
+                    }
+                    commafied.AppendLine(updatedItem);
+                }
+
+                returnString = commafied.ToString();                
+            }
+
+            return returnString;
+        }
+
+        public static string Quotify(string selectedText)
+        {
+            var returnString = selectedText;
+
+            if (selectedText != null && selectedText != String.Empty)
+            {
+                var commafied = new StringBuilder();
+                foreach (var item in selectedText.Replace("\r", "").Split('\n'))
+                {
+                    var updatedItem = item;
+                    if (item != string.Empty)
+                    {
+                        updatedItem = String.Format(@"{0}{1}{2}", "\"", item, "\"");
+                    }
+                    commafied.AppendLine(updatedItem);
+                }
+
+                returnString = commafied.ToString();
+            }
+
+            return returnString;
+        }
+    }
+}
